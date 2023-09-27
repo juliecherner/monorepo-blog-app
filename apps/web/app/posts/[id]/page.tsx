72588d0bceb800c0f5330b95dea6996f030c4dbd
juliecherner <juliechernen@gmail.com>
2023-09-27 14:08:57 +0300

    Page is condition is added

--- apps/web/app/posts/[id]/page.tsx
+++ apps/web/app/posts/[id]/page.tsx
@@ -41,49 +41,57 @@ export default function PostPage({
 
   return (
     <div className="m-4">
-      {isUserPost() && (
-        <div className="flex justify-between">
-          <div>You posted</div>
+      {data?.ok ? (
+        <div>
+          {isUserPost() && (
+            <div className="flex justify-between">
+              <div>You posted</div>
 
-          <div className="flex gap-2">
-            <button
-              className="bg-amber-500 hover:bg-amber-700 text-black font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-              onClick={() => setIsEdit(!isEdit)}
-            >
-              <BiEditAlt />
-            </button>
+              <div className="flex gap-2">
+                <button
+                  className="bg-amber-500 hover:bg-amber-700 text-black font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+                  onClick={() => setIsEdit(!isEdit)}
+                >
+                  <BiEditAlt />
+                </button>
 
-            <button
-              className="bg-red-500 hover:bg-red-700 text-black font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-              onClick={remove}
-            >
-              <AiOutlineDelete />
-            </button>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-black font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+                  onClick={remove}
+                >
+                  <AiOutlineDelete />
+                </button>
+              </div>
+            </div>
+          )}
+
+          {isEdit ? (
+            <PostForm
+              defaultTitle={data?.data?.name}
+              defaultText={data?.data?.description}
+              action={PostAction.EDIT}
+              actionButtonText="Submit edited post"
+              closeForm={() => setIsEdit(false)}
+              reload={() => mutate()}
+              id={id}
+            />
+          ) : (
+            <div>
+              <div>{data?.data?.name}</div>
+              <div>{data?.data?.description}</div>
+            </div>
+          )}
+
+          <div>
+            {isUserPost() ? <p>Your post</p> : <p>Author</p>}
+            <p>
+              {postService.getNameFromEmail(data?.data?.authorId?.username)}
+            </p>
           </div>
         </div>
-      )}
-
-      {isEdit ? (
-        <PostForm
-          defaultTitle={data?.data?.name}
-          defaultText={data?.data?.description}
-          action={PostAction.EDIT}
-          actionButtonText="Submit edited post"
-          closeForm={() => setIsEdit(false)}
-          reload={() => mutate()}
-          id={id}
-        />
       ) : (
-        <div>
-          <div>{data?.data?.name}</div>
-          <div>{data?.data?.description}</div>
-        </div>
+        <div>Something went wrong, item {id} is not found.</div>
       )}
-
-      <div>
-        {isUserPost() ? <p>Your post</p> : <p>Author</p>}
-        <p>{postService.getNameFromEmail(data?.data?.authorId?.username)}</p>
-      </div>
     </div>
   );
 }
